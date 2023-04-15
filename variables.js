a29c5a85abbcc8a9df6341f780f7817374927e76
Gustavo <gustavo_r69@yahoo.com.ar>
2023-04-15 19:39:36 -0300

    fin de la practica 1-7

---
+++ variables.js
@@ -0,0 +1,33 @@
+// var + nombre variable + = + valor literal
+
+var anioActual = 2022; // entero o int o integer 
+
+var carreteraBuenosAiresMarDelPlata = 404 // entero o int o integer
+
+var nombreMascotaPreferida = "pepote" // string o cadena de caracteres o tipo de dato palabra
+
+var calleCorrientes = "calleteatralporexcelencia" // string o cadena de caracteres o tipo de dato palabra
+
+var estaLloviendo = false; // bool o boolean o booleano
+
+var ganoBoca = false; // bool o boolean o booleano
+
+var cantBitcoinsPoseo =  0.0000024; // float o decimal 
+
+var cantDolaresPoseo = 32.32; // float o decimal
+
+// terminamos la primer parte
+
+// segunda practica
+
+document.write (`
+    <h4> año actual: ${ anioActual } </h4>
+    <h4> autopistaBuenosAiresMarDelPlata : ${ carreteraBuenosAiresMarDelPlata } </h4>
+    <h4> nombredemimascotareferida : ${ nombreMascotaPreferida} </h4>
+    <h4> lacallecorrienteseslamasgrandedeCABA : ${calleCorrientes} </h4>
+    <h4> como estara el clima hoy? true para si - false para no: ${ estaLloviendo } </h4>
+    <h4> como saldra el partido de hoy? true para si - false para no: ${ ganoBoca } </h4>
+    <h4> mis bitcoins: ${cantBitcoinsPoseo} BTC </h4>
+    <h4> mis pesos: ${cantDolaresPoseo} USD </h4>
+`);
+
